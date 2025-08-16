82954ca2bc02e20123f9c2694ffcbfbc22132969
ArashAghapour <arashaghapour13@gmail.com>
2025-08-16 15:51:16 +0330

    Replace search.py with version from searched-with-sql

--- search.py
+++ search.py
@@ -1,10 +1,13 @@
 import sqlite3 as lite
-def search_in_database(user_in_code, products, searched_item, purchase, browsed, users):
-    products_scores = {}    
-    parameters_score = {'concerns': 85, 'category': 75, 'views':63,'searched_box': 120,
-                        'rating': 51, 'name': 110, 'brand': 105, 
+import json
+
+def search_in_database(user_in_code, searched_item):
+    products_scores = {}
+    parameters_score = {'concerns': 85, 'category': 75, 'views':63,'searched_box': 301,
+                        'rating': 22, 'name': 283, 'brand': 281,
                         'purchase': 41,'searched': 13, 'close': 31,
-                        'most_view': 53, 'skin_type': 90}
+                        'most_view': 53, 'skin_type': 90, 'user_skin_type': 43, 'user_concerns': 47,
+                        'searched_skin_type': 297, 'searched_concerns': 294, 'searched_category': 292}
     
     concerns_labels = {'acne': 10,'combination': 12 ,'dullness': 14}
     category = {'cleanser': 10, 'moisturizer': 12, 'serum': 20}
@@ -21,131 +24,172 @@ def search_in_database(user_in_code, products, searched_item, purchase, browsed,
         parameters_score['most_view']: 'This is one of the most viewed products.',
         parameters_score['searched_box']: 'you exactly searched this.',
         parameters_score['views']: 'it because ite view.',
-        parameters_score['skin_type']: 'it because you have same skin types.'
+        parameters_score['skin_type']: 'it because you have same skin types.',
+        parameters_score['user_skin_type']: 'it because you have same skin types.',
+        parameters_score['user_concerns']: 'it because you have same concerns.',
+        parameters_score['searched_skin_type']: 'it because you have searched this skin types.',
+        parameters_score['searched_concerns']: 'it because you have searched this concerns.',
+        parameters_score['searched_category']: 'it because you have searched this concerns.',
     }
-    conn = lite.connect('database.db')
+
+    conn = lite.connect('./AP-Project/database.db')
     cursor = conn.cursor()
     results = []
+    output = []
     views = {}
     exist = False
     id_exist = False
-    for i in products:
-        products_scores[i['product_id']] = []
-    for i in browsed:
-        if i['product_id'] in views:
-            views[i['product_id']] += 1
-        else:
-            views[i['product_id']] = 1
-    views = sorted(views.items(), key=lambda item: item[1], reverse=True)
-    for i in products:
-        try:
-            if i['product_id'] == int(searched_item):
-                id_exist = True
-                products_scores[i['product_id']].append(parameters_score['searched_box'])
-        except:
-            pass
-        if i['name'] == searched_item:
-            products_scores[i['product_id']].append(parameters_score['searched_box'])
-            exist = True
+    for i in get_column(cursor):
+        products_scores[i] = []
+    views = users_views(cursor)
+    views = dict(sorted(views.items(), key=lambda item: item[1], reverse=True))
+    search_value = searched_item
+    results = None
 
-        elif i['brand'] == searched_item:
-            exist = True
-            products_scores[i['product_id']].append(parameters_score['brand'])
+    if search_value.isdigit():
+        cursor.execute("SELECT product_id FROM Products WHERE product_id = ?", (int(search_value),))
+        results = cursor.fetchall()
+        for i in results:
+            products_scores[i[0]].append(parameters_score['searched_box'])
+    if not results:
+        cursor.execute("SELECT product_id FROM Products WHERE name = ?", (search_value,))
+        results = cursor.fetchall()
+        for i in results:
+            products_scores[i[0]].append(parameters_score['name'])
+    if not results:
+        cursor.execute("SELECT product_id FROM Products WHERE brand = ?", (search_value,))
+        results = cursor.fetchall()
+        for i in results:
+            products_scores[i[0]].append(parameters_score['brand'])
+    if not results:
+        cursor.execute("""
+                       SELECT product_id
+                       FROM Products
+                       WHERE json_extract(concerns_targeted, '$[0]') = ?
+                       """, (search_value,))
+        results = cursor.fetchall()
+        for i in results:
+            products_scores[i[0]].append(parameters_score['searched_concerns'])
 
-        elif i['concerns_targeted'][0] == searched_item:
-            exist = True
-            products_scores[i['product_id']].append(parameters_score['concerns'])
+    if not results:
+        cursor.execute("""
+                       SELECT product_id
+                       FROM Products
+                       WHERE json_extract(category, '$[0]') = ?
+                       """, (search_value,))
+        results = cursor.fetchall()
+        for i in results:
+            products_scores[i[0]].append(parameters_score['searched_category'])
 
-        elif i['category'][0] == searched_item:
-            exist = True
-            products_scores[i['product_id']].append(parameters_score['searched_box'])
+    if not results:
+        cursor.execute("""
+                       SELECT product_id
+                       FROM Products
+                       WHERE json_extract(skin_types, '$[0]') = ?
+                       """, (search_value,))
+        results = cursor.fetchall()
+        for i in results:
+            products_scores[i[0]].append(parameters_score['searched_skin_type'])
 
-        elif i['skin_type'][0] == searched_item:
-            exist = True
-            products_scores[i['product_id']].append(parameters_score['searched_box'])
+    user_row_in_database = user_information(cursor, user_in_code)
+    skin_product = s_product(cursor, user_row_in_database[2])
+    purchase_in_database = purchase_da(cursor, user_in_code)
+    user_concern = concerns_targeted(cursor, user_row_in_database[3][0])
 
-    for user in users:
-        if user['user_id'] == user_in_code:
-            for product in products:
-                if product['skin_type'] == user['skin_type']:
-                    products_scores[product['product_id']].append(parameters_score['skin_type'])
-            break
-    for j in purchase:
-        if j['user_id'] == user_in_code:
-            products_scores[j['product_id']].append(j['quantity'] * parameters_score['purchase'])
+    if skin_product:
+        for i in skin_product:
+            products_scores[i].append(parameters_score['user_skin_type'])
+
+    if user_concern:
+        for i in user_concern:
+            products_scores[i].append(parameters_score['user_concerns'])
+
+    if purchase_in_database:
+        for i in purchase_in_database:
+            products_scores[i[2]].append(i[3] * parameters_score['purchase'])
+
     list_of_products = []
     for id, scores_list in products_scores.items():
         list_of_products.append([id, sum(scores_list)])
     list_of_products.sort(key=lambda item: item[1], reverse=True)
-    first_product = list_of_products[0][0]
-    for product in products:
-        if product['product_id'] == first_product:
-            for parameters, values in skin_types.items():
-                if abs(values - skin_types[product['skin_type'][0]]) < 30:
-                    for l in products:
-                        if l['skin_type'] == parameters:
-                            products_scores[l['product_id']].append(parameters_score['close'])
 
-            for parameters, values in category.items():
-                if abs(values - category[product['category'][0]]) < 3:
-                    for l in products:
-                        if l['category'] == parameters:
-                            products_scores[l['product_id']].append(parameters_score['close'])
+    if results or products_scores:
+        for id, scores_list in products_scores.items():
+            if id in views:
+                scores_list.append(views[id] * parameters_score['searched'])
+
+        for i, j in views.items():
+            products_scores[i].append(parameters_score['most_view'])
             break
-    if id_exist or exist:
+
         for id, scores_list in products_scores.items():
-            for item_id, view in views:
-                if item_id == id:
-                    scores_list.append(parameters_score['searched'] * view)
-        products_scores[views[0][0]].append(parameters_score['most_view'])
-        for id, scores_list in products_scores.items():
-            for item in products:
-                if item['product_id'] == id:
-                    products_scores[item['product_id']].append(parameters_score['rating'])
+            products_scores[id].append(round(product_rating(cursor, id) * parameters_score['rating'], 2))
+
         list_of_products = []
         for id, scores_list in products_scores.items():
             list_of_products.append([id, sum(scores_list)])
         list_of_products.sort(key=lambda item: item[1], reverse=True)
+
+        first_product = product_information(cursor, list_of_products[0][0])
+        near_skin_type = 30
+        for skin_type, score in skin_types.items():
+            if abs(score - skin_types[first_product[4][0]]) < near_skin_type:
+                near_products = s_product(cursor, skin_type)
+                for i in near_products:
+                    products_scores[i].append(parameters_score['close'])
+
+        near_concern = 3
+        for concern, score in concerns_labels.items():
+            if abs(score - concerns_labels[first_product[5][0]]) < near_concern:
+                product = concerns_targeted(cursor, concern)
+                for i in product:
+                    products_scores[i].append(parameters_score['close'])
+
         if len(list_of_products) > 1:
             for item in range(len(list_of_products) - 1):
                 first_item = list_of_products[item][0]
                 second_item = list_of_products[item + 1][0]
                 for score in products_scores[first_item]:
                     if score not in products_scores[second_item]:
-                        list_of_products[item].append(reasons_of_parameters[score])
+                        for i in reasons_of_parameters:
+                            if score % i == 0:
+                                list_of_products[item].append(reasons_of_parameters[i])
+                                break
                         break
-                if len(list_of_products[item]) == 2:
-                    for values, descriptions in reasons_of_parameters.items():
-                        if max(products_scores[first_item]) % values == 0:
-                            list_of_products[item].append(reasons_of_parameters[values])
             for values, descriptions in reasons_of_parameters.items():
                 if max(products_scores[list_of_products[-1][0]]) % values == 0:
                     list_of_products[-1].append(reasons_of_parameters[values])
+                else:
+                    list_of_products[-1].append(reasons_of_parameters[parameters_score['rating']])
         else:
-            list_of_products[-1].append(reasons_of_parameters[max(products_scores[list_of_products[-1][0]])])
+            for values, descriptions in reasons_of_parameters.items():
+                if max(products_scores[list_of_products[-1][0]]) % values == 0:
+                    list_of_products[-1].append(reasons_of_parameters[values])
         list_of_products.sort(key=lambda j: j[1], reverse=True)
-        print(products_scores)
         print(list_of_products)
+        print(products_scores)
+        count1 = 0
         for item in list_of_products:
             if item[1] == 0:
                 continue
-            for item_list_search in products:
-                if item_list_search['product_id'] == item[0]:
-                    item_of_product = {
-                        'product_id': item_list_search['product_id'],
-                        'name': item_list_search['name'],
-                        'brand': item_list_search['brand'],
-                        'category': item_list_search['category'],
-                        'skin_type': item_list_search['skin_type'],
-                        'concerns_targeted': item_list_search['concerns_targeted'],
-                        'ingredients': item_list_search['ingredients'],
-                        'price': item_list_search['price'],
-                        'rating': item_list_search['rating'],
-                        'response': item[2]
-                    }
-                    results.append(item_of_product)
-                    break
-        return {'items': results}
+            if count1 == 3:
+                break
+            item_list_search = product_information(cursor, item[0])
+            item_of_product = {
+                'product_id': item_list_search[0],
+                'name': item_list_search[1],
+                'brand': item_list_search[2],
+                'category': item_list_search[3],
+                'skin_type': item_list_search[4],
+                'concerns_targeted': item_list_search[5],
+                'ingredients': item_list_search[6],
+                'price': item_list_search[7],
+                'rating': item_list_search[8],
+                'response': item[2]
+            }
+            output.append(item_of_product)
+            count1 += 1
+        return {'items': output}
     else:
         return {'items': [{'product_id': user_in_code,
                            'name': 'None',
@@ -157,3 +201,50 @@ def search_in_database(user_in_code, products, searched_item, purchase, browsed,
                            'price': 0,
                            'rating': 0.0,
                            'response': 'we dont have product for your search.'}]}
+def users_views(cursor):
+    cursor.execute("""
+                   SELECT product_id, COUNT(*) AS views_count
+                   FROM Browsing_History
+                   GROUP BY product_id
+                   """)
+    return dict(cursor.fetchall())
+def user_information(cursor, user_id):
+    cursor.execute("select * from Users where user_id = ?", (user_id, ))
+    row = list(cursor.fetchone())
+    row[3] = json.loads(row[3])
+    row[4] = json.loads(row[4])
+    return list(row)
+
+def product_information(cursor, product_id):
+    cursor.execute("select * from Products where product_id = ?", (product_id, ))
+    row = list(cursor.fetchone())
+    row[3] = json.loads(row[3])
+    row[4] = json.loads(row[4])
+    row[5] = json.loads(row[5])
+    row[6] = json.loads(row[6])
+    return row
+
+def product_rating(cursor, product_id):
+    cursor.execute("select rating from Products where product_id = ?", (product_id, ))
+    return cursor.fetchone()[0]
+
+def s_product(cursor, product_type):
+    cursor.execute("select p.* from Products p, json_each(p.skin_types) where json_each.value = ?", (product_type, ))
+    row = cursor.fetchall()
+    row = [i[0] for i in row]
+    return row
+
+def purchase_da(cursor, user_in_code):
+    cursor.execute("select * from Purchase_History where user_id = ?", (user_in_code, ))
+    return cursor.fetchall()
+
+def concerns_targeted(cursor, concern):
+    cursor.execute("select p.* from Products p, json_each(p.concerns_targeted) where json_each.value = ?", (concern, ))
+    row = cursor.fetchall()
+    row = [i[0] for i in row]
+    return row
+
+def get_column(cursor):
+    cursor.execute("SELECT product_id FROM Products")
+    columns = [i[0] for i in cursor.fetchall()]
+    return columns
